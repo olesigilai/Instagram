714377bf810724c315b82f06944966fec7713570
olesigilai <ole_sigilai@yahoo.com>
2021-03-27 01:21:59 +0300

    class model Profile is created and fields are created for the attributes it will contain

--- user/models.py
+++ user/models.py
@@ -39,6 +39,42 @@ class Image (models.Model):
     def likes(self):
         return self.likes.count()
 
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(blank = True,max_length = 30)
+    email = models.CharField(blank = True, max_length = 100)
+    bio = models.TextField(max_length=100)
+    profile_image = models.ImageField(upload_to = 'images/')
+    follow = models.ManyToManyField(User, related_name='follows',blank = True)
+    
+    # def __str__(self):
+    #     return self.profile_image.url
+
+    # def save_profile(self):
+    #     self.save()
+        
+    # @classmethod
+    # def get_profile(cls,id):
+    #     profile = Profile.objects.all()
+    #     return profile
+        
+    # @classmethod
+    # def update_profile(cls,id,new_name):
+    #     cls.objects.filter(id=id).update(name = new_name)
+
+    # @classmethod
+    # def delete_profile(cls,id):
+    #     cls.objects.filter(id).delete()
+
+
+    # @classmethod
+    # def search_by_name(cls, searched_name):
+    #     username = cls.objects.filter(name__icontains=searched_name)
+
+    #     return username
+
+
+
 class Comments(models.Model):
     comment = models.TextField(blank = True, max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE,blank = True)
@@ -65,6 +101,6 @@ class Comments(models.Model):
     @classmethod
     def delete_comment(cls,id):
         cls.objects.filter(id).delete()
-    
+ 
 
     
