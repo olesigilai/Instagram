447b26eddcc740293ee2a633baca583b0b956f97
olesigilai <ole_sigilai@yahoo.com>
2021-03-27 01:18:16 +0300

    classmethod that saves and that which updates comments are created

--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import cloudinary
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
-
+from django .utils import timezone
 # Create your models here.
 class Image (models.Model):
     image = CloudinaryField('images',blank = True)
@@ -11,7 +11,7 @@ class Image (models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     profile = models.ForeignKey('Profile', on_delete = models.CASCADE)
     likes = models.PositiveIntegerField(defaiult = 0)
-
+    date_posted = models.DateTimeField(default=timezone)
     def __str__(self):
         return self.image_name
     
@@ -38,8 +38,33 @@ class Image (models.Model):
 
     def likes(self):
         return self.likes.count()
-        
 
+class Comments(models.Model):
+    comment = models.TextField(blank = True, max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank = True)
+    image = models.ForeignKey(Image, blank=True, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)     
+
+    def __str__(self):
+        return self.comment
+
+    def save_comment(self):
+        self.save()
+    
+    
+    @classmethod
+    def update_comment(cls,id,new_comment):
+        cls.objects.filter(id=id).update(comment = new_comment)
+        
+    # @classmethod
+    # def get_comments(cls,id):
+    #     comments = cls.objects.filter(image__id=id)
+    #     return comments
+    
+
+    # @classmethod
+    # def delete_comment(cls,id):
+    #     cls.objects.filter(id).delete()
     
 
     
