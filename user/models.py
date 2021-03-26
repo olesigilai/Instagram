9ed2dc74ea0565261cfa4bddcfc93356e9c9865e
olesigilai <ole_sigilai@yahoo.com>
2021-03-26 22:46:29 +0300

    class Image model is created and fields provided

--- user/models.py
+++ user/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Image (models.Model):
-    def 
+    image = CloudinaryField('images',blank = True)
+    image_name = models.CharField(max_length=55)
+    image_caption = models.CharField(max_length=55)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete = models.CASCADE)
+    likes = models.PositiveIntegerField(defaiult = 0)
+
