e952d102831b079930f7c290af02d930692c9533
olesigilai <ole_sigilai@yahoo.com>
2021-03-28 14:57:27 +0300

    crispy forms added to installed apps

--- instagram/settings.py
+++ instagram/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'bootstrap3',
     'user',
     'cloudinary',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
