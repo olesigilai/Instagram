1ff0e5b158fbcd18063a01997f020d1250d270ca
olesigilai <ole_sigilai@yahoo.com>
2021-03-30 01:09:20 +0300

    deployment to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -40,7 +40,7 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
